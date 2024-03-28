31cfa2e31019b2c97a0ed3ecd4d320c44a734800
wanyanchengyu <150008747+ylxcvvan@users.noreply.github.com>
2024-03-28 22:34:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '-5o2EgnjszEDQuKVsO-hP4CdmV5R7wrnQaRqmIO0-lI',
 
   CALLBACK_USERS: [
     {
