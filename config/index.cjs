120fa0d4c4121226c1ed35b3c211cfe963c0ed36
wanyanchengyu <150008747+ylxcvvan@users.noreply.github.com>
2024-03-29 16:55:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_Yve6Te7hjIzCnIpeGByFNdcG_I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 's4tZoiLGrmlt0mdrre6jMLbqIW1Zm1tZ5wCSEpFrUp0',
+      useTemplateId: 'vpT9pJZ3EZbunpG3CjeXbLmYwT11iTRdLFbKVtjENyg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-12',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 's4tZoiLGrmlt0mdrre6jMLbqIW1Zm1tZ5wCSEpFrUp0',
+  CALLBACK_TEMPLATE_ID: 'vpT9pJZ3EZbunpG3CjeXbLmYwT11iTRdLFbKVtjENyg',
 
   CALLBACK_USERS: [
     {
